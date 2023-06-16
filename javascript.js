9c1865824970fd6716ca59e5c96753f8ac364edc
nicolas293 <nechepaevVICLS58@gmail.com>
2023-06-16 08:00:33 +0300

    Main file new

--- javascript.js
+++ javascript.js
@@ -2,12 +2,12 @@ function onButtonClick() {
   var mainSid1 = document.getElementById("mainSid1");
   var nav = document.getElementById("nav");
   
-    if(mainSid1.style.display == "none") {
-       mainSid1.style.display = "none";
-       mainSid1.style.display = "inline"; 
+    if(mainSid1.style.display == "block") {
+       mainSid1.style.display = 'block';
+       mainSid1.style.display = 'none'; 
     } else {
-       mainSid1.style.display = "inline";
-       mainSid1.style.display = "none"; 
+       mainSid1.style.display = 'none';
+       mainSid1.style.display = 'block'; 
     }
   
 }
